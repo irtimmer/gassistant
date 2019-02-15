ad08ffc6a5b9a12e5798ce05125286a6e697158d
Iwan Timmer <irtimmer@gmail.com>
2019-02-15 20:11:59 +0100

    Initial commit

---
+++ gassistant/handler.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2019 Iwan Timmer
+# Copyright (C) 2017 Google Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+from google.assistant.library.event import EventType
+
+def process_event(event):
+    print(event)
